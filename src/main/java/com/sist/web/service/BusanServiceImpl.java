abe5c7c86afdb2c8a11ec8623a98b1227d725e70
jieunpyo <jieunhye1121@naver.com>
2025-12-24 09:19:18 +0900

    1111

---
+++ src/main/java/com/sist/web/service/BusanServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sist.web.service;
+import java.util.*;
+
+import org.springframework.stereotype.Service;
+
+import com.sist.web.mapper.*;
+import com.sist.web.vo.*;
+
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
+public class BusanServiceImpl implements BusanService{
+   private final BusanMapper mapper;
+
+   @Override
+   public List<BusanVO> busanListData(Map map) {
+	// TODO Auto-generated method stub
+	return mapper.busanListData(map);
+   }
+
+   @Override
+   public int busanTotalPage(Map map) {
+	// TODO Auto-generated method stub
+	return mapper.busanTotalPage(map);
+   }
+
+}
